bb2cf56a92a608cf3b3cd46f770926d620f94888
Sandile <sandilengeleka97@gmail.com>
2026-01-05 13:35:29 +0200

    Data seeding

--- GameStore.Api/Data/Migrations/DataExtensions.cs
+++ GameStore.Api/Data/Migrations/DataExtensions.cs
@@ -1,3 +1,4 @@
+using GameStore.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data.Migrations;
@@ -10,5 +11,29 @@ public static class DataExtensions
         var dbContext = scope.ServiceProvider
                              .GetRequiredService<GameStoreContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static void AddGameStoreDb(this WebApplicationBuilder builder)
+    {
+        var connString = "Data Source=GameStore.db";
+        builder.Services.AddSqlite<GameStoreContext>(
+            connString,
+            optionsAction: options => options.UseSeeding((context, _) =>
+            {
+                if (!context.Set<Genre>().Any())
+                {
+                    context.Set<Genre>().AddRange(
+                        new Genre { Name = "Fighting"},
+                        new Genre { Name = "RPG"},
+                        new Genre { Name = "Platformer"},
+                        new Genre { Name = "Racing"},
+                        new Genre { Name = "Sports"}
+                    );
+
+                    context.SaveChanges();
+                }
+            })
+            
+        );
     }
 }
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -2,14 +2,15 @@ using GameStore.Api.Data;
 using GameStore.Api.Data.Migrations;
 using GameStore.Api.Dtos;
 using GameStore.Api.endpoints;
+using GameStore.Api.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
-var connString = "Data Source=GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString);
+builder.AddGameStoreDb();
 
 var app = builder.Build();
 
